b6bc5d98755d7d020bdcba0fa5a40fc8182c0ff4
Josh Nussbaum <joshnuss@gmail.com>
2022-09-27 01:44:21 -0400

    Formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-	root: true,
-	extends: ['eslint:recommended', 'prettier', 'plugin:vitest-globals/recommended'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true,
-		"vitest-globals/env": true
-	}
+  root: true,
+  extends: ['eslint:recommended', 'prettier', 'plugin:vitest-globals/recommended'],
+  plugins: ['svelte3'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+    'vitest-globals/env': true
+  }
 };
--- vitest.config.js
+++ vitest.config.js
@@ -1,15 +1,18 @@
-import { mergeConfig, defineConfig } from 'vite'
-import viteConfig from './vite.config'
-import path from 'path'
+import { mergeConfig, defineConfig } from 'vite';
+import viteConfig from './vite.config';
+import path from 'path';
 
-export default mergeConfig(viteConfig, defineConfig({
-	test: {
-		globals: true,
-		setupFiles: './test/setup.js'
-	},
-	resolve: {
-		alias: {
-			'$routes': path.resolve(__dirname, 'src/routes')
-		}
-	}
-}))
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      setupFiles: './test/setup.js'
+    },
+    resolve: {
+      alias: {
+        $routes: path.resolve(__dirname, 'src/routes')
+      }
+    }
+  })
+);
