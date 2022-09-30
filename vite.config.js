bfb6d0ac20982c7c9bd96a4729d28fffee060d6e
Josh Nussbaum <joshnuss@gmail.com>
2022-09-29 23:53:34 -0400

    Adds config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import path from 'path'
 
 const config = {
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $config: path.resolve(__dirname, 'config.js'),
+    }
+  }
 }
 
 export default config
