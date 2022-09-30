1f315831a3973eb8d28f4f1fd9e9c2fa53f26c44
Josh Nussbaum <joshnuss@gmail.com>
2022-09-30 04:51:44 -0400

    Extract cart middleware

--- src/routes/cart/[id]/+server.js
+++ src/routes/cart/[id]/+server.js
@@ -2,13 +2,19 @@ import { error } from '@sveltejs/kit'
 import serialize from '$lib/serializers/cart'
 import * as service from '$lib/services/cart'
 
-export async function GET({ params, request }) {
-  const token = request.headers.get('authorization')
-  const cart = await service.get({ publicId: params.id, token })
-
-  if (!cart) {
-    throw error(401)
-  }
-
+export const GET = withCart(cart => {
   return serialize(cart)
+})
+
+function withCart(callback) {
+  return async ({request, params}) => {
+    const token = request.headers.get('authorization')
+    const cart = await service.get({ publicId: params.id, token })
+
+    if (!cart) {
+      throw error(401)
+    }
+
+    return callback(cart)
+  }
 }
