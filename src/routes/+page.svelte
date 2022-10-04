edd28b8794ee3f939cd13f3927aa84e4d6b24379
Josh Nussbaum <joshnuss@gmail.com>
2022-10-04 17:00:08 -0400

    Fix broken link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
 <p>Cloud-agnostic shopping cart using Stripe Checkout.</p>
 <p>Reference design for SvelteKit + Prisma apps.</p>
 <p>
-  <a href="https://github.com/joshnuss/sky-carti#readme">API documentation</a>
+  <a href="https://github.com/joshnuss/sky-cart#readme">API documentation</a>
 </p>
